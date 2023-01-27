23ff796bc73d88e982f0e846f57be753fd97aa53
@Snick55 <snikerss123321@gmail.com>
2023-01-27 15:55:40 +0300

    added the ability to view traffic

--- app/src/main/java/com/android/loginapp/maps/presentation/MapsFragment.kt
+++ app/src/main/java/com/android/loginapp/maps/presentation/MapsFragment.kt
@@ -1,9 +1,13 @@
 package com.android.loginapp.maps.presentation
 
 import android.app.AlertDialog
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.Fragment
 import com.android.loginapp.R
 import com.android.loginapp.core.App
@@ -45,6 +49,7 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (requireContext().applicationContext as App).mapsViewModel
         (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        val traffic = MapKitFactory.getInstance().createTrafficLayer(binding.mapView.mapWindow)
         viewModel.getUsername()
         viewModel.getLocation()
 
@@ -58,7 +63,19 @@ class MapsFragment : Fragment() {
             )
         }
 
+        var isTrafficActive = false
         binding.findMeButton.setOnClickListener { viewModel.getLocation() }
+        binding.trafficButton.setOnClickListener {
+            it.setBackgroundResource(R.drawable.ic_traffic_green)
+            if (!isTrafficActive) {
+                traffic.isTrafficVisible = true
+                isTrafficActive = !isTrafficActive
+            } else {
+                it.setBackgroundResource(R.drawable.ic_traffic)
+                traffic.isTrafficVisible = false
+                isTrafficActive = !isTrafficActive
+            }
+        }
     }
 
 
